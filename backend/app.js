bb4c53e59801eaf2a6e3d9ca9e9cc3f76fa659d1
jayr6741 <146107060+jayr6741@users.noreply.github.com>
2025-05-23 17:03:28 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const dotenv = require("dotenv");
 const mongodb = require("./library/db");
 const v1 = require("./routes/v1");
-const swagger = require("./swagger-ui/swagger");
-
-dotenv.config();
-
+const swegger = require("./swagger-ui/swagger");
 const app = express();
+require("dotenv").config();
 app.use(express.json());
 app.use(cors());
-
-// Static route for uploaded images
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
 app.use("/v1", v1);
-app.use("/swagger", swagger);
-
+app.use("/swegger", swegger);
 mongodb();
 
+
 app.listen(process.env.PORT, () => {
-  console.log("Server started on port", process.env.PORT);
+  console.log("start server", process.env.PORT);
 });
