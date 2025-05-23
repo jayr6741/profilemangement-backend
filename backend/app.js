ce18b3d64ad444f94b5d78db70267eb18a38d462
jayr6741 <146107060+jayr6741@users.noreply.github.com>
2025-05-23 17:08:43 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const mongodb = require("./library/db");
-const v1 = require("./routes/v1");
-const swegger = require("./swagger-ui/swagger");
-const app = express();
-require("dotenv").config();
-app.use(express.json());
-app.use(cors());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/v1", v1);
-app.use("/swegger", swegger);
-mongodb();
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const mongodb = require('./library/db')
+const v1 = require('./routes/v1')
+const swegger = require('./swagger-ui/swagger')
+const app = express()
+require('dotenv').config()
+app.use(express.json())
+app.use(cors())
+app.use("/v1", v1)
+app.use("/swegger",swegger)
+// beforeAll(async () => {
+//     await mongodb(); // ✅ Wait for DB connection before tests
+//   });
+  
+mongodb()
+app.use('/uploads', express.static(path.join(__dirname, './uploads')));
+
+
 
 
 app.listen(process.env.PORT, () => {
-  console.log("start server", process.env.PORT);
-});
+    console.log("start server", process.env.PORT);
+})
