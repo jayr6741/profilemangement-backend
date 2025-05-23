4fda55e3315eec91dbfcc88479330eaca5f37858
jayr6741 <146107060+jayr6741@users.noreply.github.com>
2025-05-23 17:15:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,13 @@ const cors = require('cors')
 const path = require('path')
 const mongodb = require('./library/db')
 const v1 = require('./routes/v1')
-const swegger = require('./swagger-ui/swagger')
+
 const app = express()
 require('dotenv').config()
 app.use(express.json())
 app.use(cors())
 app.use("/v1", v1)
-app.use("/swegger",swegger)
+
 mongodb()
 
 app.use('/uploads', express.static(path.join(__dirname, './uploads')));
